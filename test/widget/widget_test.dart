a43d14eeb20f2c7c3432f692ce11d806e29a5c07
zainulabidin-codes <zainulabidinamirsaeed9@gmail.com>
2026-08-31 09:21:32 +0500

    chore: remove dummy test and relocate non-test tooling out of test/

--- test/widget/widget_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Long press deletes cow placeholder', (WidgetTester tester) async {
-    expect(true, isTrue);
-  });
-}
